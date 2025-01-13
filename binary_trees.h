156cf8c963647ab31de3c86de20355dde3a07213
Yacine-213 <d.yacine2020@gmail.com>
2025-01-13 09:46:34 +0000

    18

--- binary_trees.h
+++ binary_trees.h
@@ -15,7 +15,7 @@ typedef struct binary_tree_s
 
 /* Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
